4fc4c9732c55a18cbca87c8bf3dd019d25716f7b
Netwave <3danimanimal@gmail.com>
2015-10-03 19:15:03 +0800

    added load image functionality
    not scaling properly, but loading image connected to the open action
    menu.

--- sampleapp.cpp
+++ sampleapp.cpp
@@ -1,14 +1,41 @@
 #include "sampleapp.h"
 #include "ui_sampleapp.h"
+#include <QFileDialog>
+#include <QImage>
 
 SampleApp::SampleApp(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::SampleApp)
 {
-    ui->setupUi(this);
+    ui          ->  setupUi(this);
+    scene       =   new QGraphicsScene();
+    img_item    =   nullptr;
+    img         =   new QPixmap();
+    ui->image_view->setScene(scene);
+
+    connect(ui->actionOpen, &QAction::triggered, [this](){this->loadImage();});
 }
 
 SampleApp::~SampleApp()
 {
     delete ui;
+}
+
+QString SampleApp::get_filename_from_UI()
+{
+    return QFileDialog::getOpenFileName(this,
+                                        tr("Open Image"),
+                                        "",
+                                        tr("Image Files(*.png *.jpg *.bmp)"));
+}
+
+void SampleApp::loadImage()
+{
+    img->load(get_filename_from_UI());
+    if(img_item != nullptr) img_item->setPixmap(*img);
+    else
+    {
+        img_item = scene->addPixmap(*img);
+        scene->addItem(img_item);
+    }
 }
--- sampleapp.h
+++ sampleapp.h
@@ -2,6 +2,11 @@
 #define SAMPLEAPP_H
 
 #include <QMainWindow>
+#include <QString>
+#include <QGraphicsScene>
+#include <QPixmap>
+#include <QGraphicsPixmapItem>
+
 
 namespace Ui {
 class SampleApp;
@@ -14,9 +19,15 @@ class SampleApp : public QMainWindow
 public:
     explicit SampleApp(QWidget *parent = 0);
     ~SampleApp();
+    void loadImage();
 
 private:
-    Ui::SampleApp *ui;
+    Ui::SampleApp           *ui;
+    QGraphicsScene          *scene;
+    QGraphicsPixmapItem     *img_item;
+    QPixmap                 *img;
+
+    QString                 get_filename_from_UI();
 };
 
 #endif // SAMPLEAPP_H
